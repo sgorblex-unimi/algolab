ac8303607a69aa22d3a6905b2842ba68b18feddf
Alessandro Clerici <alessandro.clericilorenzini@studenti.unimi.it>
2020-11-27 12:23:31 +0100

    bitree interface: added documentation

--- include/bistree.h
+++ include/bistree.h
@@ -2,19 +2,29 @@
 #define BISTREE_H
 #include "bitree.h"
 
+// Interface for creating and interacting with binary search trees.
+
+// This interface is based on the one contained in Kyle Loudon's "Mastering algorithms with C", published by O'Reilly in 1999. I adapted it to work with generic binary search trees, while the original used AVLs.
+
 typedef BiTree BisTree;
 
+// Initializes tree. It must be called before the tree can be used in any other operation. The function pointer compare specifies a user-defined function to compare elements. This function's return value should be positive if key1 > key2, 0 if key1 = key2, and negative if key1 < key2. The destroy function pointer provides a way to free dynamically allocated data when bistree_destroy is called.
 void bistree_init(BisTree *tree, int (*compare)(const void *key1, const void *key2), void (*destroy)(void *data));
 
+// Destroys tree. No other operations are permitted after calling bistree_destroy unless bistree_init is called again. If the destroy function pointer was set to not NULL when initializing the tree, then it will be used to destroy all data related to tree's nodes.
 void bistree_destroy(BisTree *tree);
 
+// Inserts a node containing data in tree. It uses the binary search trees criteria to determine where to put the new node. Returns 0 if inserting the node is successful, -1 otherwise. It is the responsibility of the caller to manage the storage associated with data.
 int bistree_insert(BisTree *tree, const void *data);
 
 // [ NOT IMPLEMENTED YET ]
+// Removes a node matching data from tree. If the destroy function pointer was set to not NULL when initializing the tree, then it will be used to destroy the data related to the removed node.
 int bistree_remove(BisTree *tree, const void *data);
 
+// Determines whether a node matches data's pointed data in tree. If a match is found, after the operation data points to the matching data in the binary search tree upon return.
 int bistree_lookup(BisTree *tree, void **data);
 
+// Evaluates to the number of nodes in tree.
 #define bistree_size(tree) ((tree)->size)
 
 #endif
--- include/bitree.h
+++ include/bitree.h
@@ -2,6 +2,10 @@
 #define BITREE_H
 #include <stdlib.h>
 
+// Interface for creating and interacting with binary trees.
+
+// This interface is based on the one contained in Kyle Loudon's "Mastering algorithms with C", published by O'Reilly in 1999. I added functions to perform ordered visit and conversion from an array.
+
 typedef struct BiTreeNode_ {
 	void *data;
 	struct BiTreeNode_ *left;
@@ -15,40 +19,58 @@ typedef struct BiTree_ {
 	BiTreeNode *root;
 } BiTree;
 
+// Initializes tree. It must be called before the tree can be used in any other operation. The destroy function pointer provides a way to free dynamically allocated data when bistree_destroy is called.
 void bitree_init(BiTree *tree, void (*destroy)(void *data));
 
+// Destroys tree. No other operations are permitted after calling bistree_destroy unless bistree_init is called again. If the destroy function pointer was set to not NULL when initializing the tree, then it will be used to destroy all data related to tree's nodes.
 void bitree_destroy(BiTree *tree);
 
+//  Inserts a node containing data as the left child of node in tree or as the root if node is NULL. Returns 0 if inserting the node is successful, -1 otherwise. It is the responsibility of the caller to manage the storage associated with data.
 int bitree_ins_left(BiTree *tree, BiTreeNode *node, const void *data);
 
+//  Inserts a node containing data as the right child of node in tree or as the root if node is NULL. Returns 0 if inserting the node is successful, -1 otherwise. It is the responsibility of the caller to manage the storage associated with data.
 int bitree_ins_right(BiTree *tree, BiTreeNode *node, const void *data);
 
+// Removes the subtree of tree rooted at the left child of node. If node is NULL, all nodes in the tree are removed. If the destroy function pointer was set to not NULL when initializing the tree, then it will be used to destroy all data related to the subtree's nodes.
 void bitree_rem_left(BiTree *tree, BiTreeNode *node);
 
+// Removes the subtree of tree rooted at the right child of node. If node is NULL, all nodes in the tree are removed. If the destroy function pointer was set to not NULL when initializing the tree, then it will be used to destroy all data related to the subtree's nodes.
 void bitree_rem_right(BiTree *tree, BiTreeNode *node);
 
+// Merges left and right as subtrees of merge. After the operation merge's root node will contain data and left and right will be destroyed.
 int bitree_merge(BiTree *merge, BiTree *left, BiTree *right, const void *data);
 
+// Executes the operation given by the function pointer action on the data of each of the nodes of tree, with the in-oder pattern.
 void bitree_inorder(BiTree *tree, void (*action)(void *data));
 
+// Executes the operation given by the function pointer action on the data of each of the nodes of tree, with the preorder pattern.
 void bitree_preorder(BiTree *tree, void (*action)(void *data));
 
+// Executes the operation given by the function pointer action on the data of each of the nodes of tree, with the postorder pattern.
 void bitree_postorder(BiTree *tree, void (*action)(void *data));
 
-int bitree_fromarr(BiTree *tree, void **arr, int size);
+// Constructs tree so that its nodes contain the values from the array arr, of size len. In the result the node containing the entry of array index i will have its children containing the data of array index 2*i+1 (left) and 2*i+2 (right).
+int bitree_fromarr(BiTree *tree, void **arr, int len);
 
+// Evaluates to the number of nodes in tree.
 #define bitree_size(tree) ((tree)->size)
 
+// Evaluates to tree's root node.
 #define bitree_root(tree) ((tree)->root)
 
+// Returns 1 if node marks the end of a branch, 0 otherwise.
 #define bitree_is_eob(node) ((node) == NULL)
 
+// Returns 1 if node is a leaf node, 0 otherwise.
 #define bitree_is_leaf(node) ((node)->left == NULL && (node)->right == NULL)
 
+// Evaluates to the data stored in node.
 #define bitree_data(node) ((node)->data)
 
+// Evaluates to a pointer to the left child of node.
 #define bitree_left(node) ((node)->left)
 
+// Evaluates to a pointer to the right child of node.
 #define bitree_right(node) ((node)->right)
 
 #endif
